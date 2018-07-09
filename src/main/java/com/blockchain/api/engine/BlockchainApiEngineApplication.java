abccb37a9998b6131c0ba6456e73a454610f2896
aqqbtgamer <30305701+aqqbtgamer@users.noreply.github.com>
2018-07-09 15:33:07 +0800

    init commit

---
+++ src/main/java/com/blockchain/api/engine/BlockchainApiEngineApplication.java
@@ -0,0 +1,12 @@
+package com.blockchain.api.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainApiEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainApiEngineApplication.class, args);
+	}
+}
---
+++ src/test/java/com/blockchain/api/engine/BlockchainApiEngineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blockchain.api.engine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlockchainApiEngineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
